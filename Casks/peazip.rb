cdee8c46490fda4b98d661c10a541e12ae32638d
Rajshekhar K <shekhar.3265@gmail.com>
2025-12-27 18:15:35 +0530

    update config to fetch the latest release

--- Casks/peazip.rb
+++ Casks/peazip.rb
@@ -1,14 +1,26 @@
 cask "peazip" do
-  version "9.2.0"
-  sha256 "4116f990e017ed2aaf4283338ee957feb5a7df2aabcf49a5ee16ec863429817e"
+  # Use :latest to tell homebrew that this will always return the newest version, and there isn't a specific version number available.
+  version :latest
+  # Use :no_check to tell Homebrew that it can't know the checksum in advance, and so it should not try to validate the checksum of the downloaded archive.
+  sha256 :no_check
 
-  # github.com/rogeriopradoj/homebrew-cask-peazip/ was verified as official when first introduced to the cask
-  url "https://github.com/peazip/PeaZip/releases/download/9.2.0/peazip-9.2.0.DARWIN.aarch64.dmg"
-  name "PeaZip"
-  homepage "https://peazip.github.io/"
+  desc "trying to get the latest release of peazip from it's GitHub page" 
+  hompage "https://github.com/v-sukt/homebrew-cask-peazip"
+  # If there's no arguments and only a block, Homebrew will wait to run the block until it actually needs the URL to download the file at install-time.
+  url do
+    # Homebrew has a built-in GitHub API client, conveniently able to provide the list of releases, converted from JSON to Ruby hashes.
+    assets = GitHub.get_release("peazip", "PeaZip", "latest").fetch("assets")
+    latest = assets.find{|a| a["name"] == "DARWIN.aarch64.dmg" }.fetch("url")
+    # The return value must match the arguments for the non-block version of `url`, first a URL, and then an options hash. The `header` option can take an array if you need to provide more than one header.
+    [latest, header: [
+      # The GitHub API will return the binary content of an asset instead of JSON data about that asset if you set the Accept header to application/octet-stream.
+      "Accept: application/octet-stream",
+      # Homebrew also has a built-in helper that will return GitHub credentials, checking the keychain, config files, gh CLI tool, and other locations automatically. We can re-use those same credentials that Homebrew uses to make API requests for our own download by setting this header.
+      "Authorization: bearer #{GitHub::API.credentials}"
+    ]]
+  end
 
   app "peazip.app"
-
   caveats <<~EOS
     1. You may need to run the following command to use PeaZip.app (*):
 
